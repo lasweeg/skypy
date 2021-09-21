edb0a4185331a53fcb10272295e0ad64c73c53dd
FuchsCrafter <69680700+FuchsCrafter@users.noreply.github.com>
2021-09-21 16:28:44 +0200

    Add bazaar class

--- skypy/main.py
+++ skypy/main.py
@@ -1,3 +1,24 @@
+import requests
+import json
+
 class skypy:
   def __init__(self):
     pass
+
+
+  class bazaar:
+    def __init__(self):
+      pass
+
+    def fetchAllProducts(self):
+      r = requests.get("https://api.hypixel.net/skyblock/bazaar")
+      return json.loads(r.text)
+
+    def fetchProduct(self, itemname):
+      r = requests.get("https://api.hypixel.net/skyblock/bazaar")
+      bazaarProducts = json.loads(r.text)
+      bazaarProducts2 = bazaarProducts["products"]
+      try:
+        return bazaarProducts2[itemname]
+      except:
+        return False
