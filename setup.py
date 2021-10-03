17f63d0ba814decc00265c44846e2240348e0a79
FuchsCrafter <69680700+FuchsCrafter@users.noreply.github.com>
2021-10-03 17:10:56 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setuptools.setup(
     name="skypy-api",
     url="https://github.com/FuchsCrafter/skypy",
-    version="0.0.1",
+    version="0.2.0",
     author="FuchsCrafter",
-    license="MIT",
+    license="GPL2",
     description="Framework to connect to the Hypixel Skyblock API",
-    long_description="Framework to connect to the Hypixel Skyblock API",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -16,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent"
     ],
     python_requires='>=3.7',
-    install_requires=["requests"]
+    install_requires=["requests, os, sys, json"]
 )
